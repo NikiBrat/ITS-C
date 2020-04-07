54823d9120cc2034423158b18965a366218cae69
Aleksej Komnenović <57869668+komnen0v1c@users.noreply.github.com>
2020-04-07 20:10:58 +0200

    testing

--- P i O pravougaonika.c
+++ P i O pravougaonika.c
@@ -11,25 +11,8 @@ main ()
 
 _________________________________________________________________________
 
-#include <stdio.h>
-
-int main() {
-
-    int stranica_a, stranica_b, povrsina, obim;
-
-    printf("Unesite stranicu a:\n");
-    scanf("%d", &stranica_a);
-
-    printf("Unesite stranicu b:\n");
-    scanf("%d", &stranica_b);
-
-    povrsina = stranica_a * stranica_b;
-    obim = stranica_a * 2 + stranica_b * 2;
-
-    printf("Povrsina je: %d\n, obim je: %d\n", povrsina, obim);
-
-    return 0;
-}
+test123
+test123313
 
 _____________________________________________________________________________
 
